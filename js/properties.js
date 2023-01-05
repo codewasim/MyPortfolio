f4b0bb4f461e6685f5fae96661ef4790726a2399
codewasim <2ab18cs028@anjuman.edu.in>
2023-01-05 20:24:52 +0530

    update wasi

---
+++ js/properties.js
@@ -0,0 +1,29 @@
+
+// 1. The title of the page here.
+document.getElementById('title').innerHTML = 'Wasim Ansari';
+
+//  2. The inner title
+document.getElementById('inner_title').innerHTML = '&nbspWasim&#x27s Portfolio';
+
+// 3. Button title
+document.getElementById('button_title').innerHTML = 'Print';
+
+// =============================================================================================
+
+// 1. User Name
+document.getElementById('user_name').innerHTML = 'Wasim Ansari';
+// 2. User Role 1
+document.getElementById('user_role_1').innerHTML = 'Salesforce Administrator';
+// 3. User Role 2
+document.getElementById('user_role_2').innerHTML = 'Salesforce Platform App Builder';
+// 3. User Role 3
+document.getElementById('user_role_3').innerHTML = 'JavaScript Developer';
+// 4. User Phone Number
+document.getElementById('user_phone').innerHTML = '&nbsp +91 7044133855';
+// 4. User Email Address
+document.getElementById('user_email').innerHTML = '&nbsp imailwsm@gmail.com';
+
+
+
+// about me
+document.getElementById('about_me').innerHTML = 'An enthusiastic programmer, who loves developing Android Application, Websites, Web Applications and coding. Interested in Artificial Intellegence and Machine learning with a positive gesture towards Application Development.';
